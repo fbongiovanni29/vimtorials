619b8a3e1a0783234642b4dac833b08822eaae10
Francis Bongiovanni <fbongiovanni29@gmail.com>
2015-11-08 16:43:06 -0500

    Refactored start.rb into test.rb

--- test.rb
+++ test.rb
@@ -1,22 +1,29 @@
-
-rb = [Dir["one.*"][0], Dir["two.*"][0], Dir["three.*"][0]]
-md = ['tut_one.md', 'tut_two.md', 'tut_three.md']
+file = [Dir["one.*"][0], Dir["two.*"][0], Dir["three.*"][0]]
+page = VIM::Buffer.current
+page = page.name.split('/').last
+if page == "README.md"	
+	VIM.command("vi tut_one.md")
+	VIM.command("vs")
+	VIM.command("vertical resize 75")
+	VIM.command("vi #{Dir["one.*"][0]}")
+	VIM.command("wincmd h")
+end
 
 if VIM::Window.current == VIM::Window[1] 
 	VIM.command("wincmd h")
 	VIM.command("ruby load 'test.rb'")
 end
 
-page = VIM::Buffer.current
-page = page.name.split('/').last
-elem = rb.index(page)
+elem = file.index(page)
+tut = ['tut_one.md', 'tut_two.md', 'tut_three.md']
+
 VIM.command("vertical resize 75")
-VIM.command("vi #{rb[(elem+1)%3]}")
+VIM.command("vi #{file[(elem+1)%3]}")
 VIM.command("wincmd l")
-VIM.command("vi #{md[(elem+1)%3]}")
+VIM.command("vi #{tut[(elem+1)%3]}")
 VIM.command("wincmd h")
 
-if rb[elem].split('.')[1].to_s == "open"
-	`open #{rb[elem]}`
+if file[elem].split('.')[1].to_s == "open"
+	`open #{file[elem]}`
 end
 
