0ebc2adb278801ac5e19651fcc829204a3b2e642
Francis Bongiovanni <fbongiovanni29@gmail.com>
2015-11-09 23:43:50 -0500

    switched filename inside  next.rb

--- next.rb
+++ next.rb
@@ -1,6 +1,13 @@
+#Selects file in directory with filename and any extension
 file = [Dir["one.*"][0], Dir["two.*"][0], Dir["three.*"][0]]
+tut = ['tut_one.md', 'tut_two.md', 'tut_three.md']
+#Sets the variable page to the current buffer
 page = VIM::Buffer.current
+
+#Splits page to extract just the filename from the full filepath
 page = page.name.split('/').last
+
+#Checks to see if it needs to start tutorial
 if page == "README.md"	
 	VIM.command("vi tut_one.md")
 	VIM.command("vs")
@@ -9,20 +16,22 @@ if page == "README.md"
 	VIM.command("wincmd h")
 end
 
+#Checks to see if the current window is on the right side
+#If true switches to left side and reloads
 if VIM::Window.current == VIM::Window[1] 
 	VIM.command("wincmd h")
-	VIM.command("ruby load 'test.rb'")
+	VIM.command("ruby load 'next.rb'")
 end
 
+#Sets the file to the index of the current page
 elem = file.index(page)
-tut = ['tut_one.md', 'tut_two.md', 'tut_three.md']
-
 VIM.command("vertical resize 75")
 VIM.command("vi #{file[(elem+1)%3]}")
 VIM.command("wincmd l")
 VIM.command("vi #{tut[(elem+1)%3]}")
 VIM.command("wincmd h")
 
+#if the file has file.open.ext open file
 if file[elem].split('.')[1].to_s == "open"
 	`open #{file[elem]}`
 end
